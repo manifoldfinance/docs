ad526d4f77e39f7d676aac5595fe1edaaa8309d9
sam bacha <sam@manifoldfinance.com>
2021-10-17 20:05:46 -0700

    Deployed d666fdf with MkDocs version: 1.2.2

---
+++ assets/custom/custom_theme.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+  displayLatestWarning();
+//  makeImagesZoomable();
+});
+
+$(window).on('hashchange', function(){
+  displayLatestWarning();
+});
+
+function displayLatestWarning(){
+  $( ".latest-warning" ).css( "display", $(location).attr('href').includes(latestWarningTrigger)?"block":"none" );
+}
+/*
+function makeImagesZoomable(){
+  $(':not(a) > img').each(function (index, value){
+    $(this).zoomify();
+  });
+}
+*/
